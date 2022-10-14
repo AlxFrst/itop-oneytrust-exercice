3eb3de17e0d7c40fb3713c2471a9712c8484fd32
AlxFrst <alexandreforestier1107@gmail.com>
2022-10-14 10:54:21 +0000

    Adding some EN-FR translation

--- en.dict.oneytrust-exercice.php
+++ en.dict.oneytrust-exercice.php
@@ -8,5 +8,11 @@
 
 Dict::Add('EN US', 'English', 'English', array(
 	// Dictionary entries go here
+	'Class:UserRequest/Stimulus:ev_attente_client' => 'Waiting for customer',
+	'Class:UserRequest/Stimulus:ev_attente_fournisseur' => 'Waiting for supplier',
+	'Class:UserRequest/Stimulus:ev_attente_atelier' => 'Waiting for workshop',
+	'Class:UserRequest/Attribute:status/Value:attente_atelier' => 'Waiting for workshop',
+	'Class:UserRequest/Attribute:status/Value:attente_fournisseur' => 'Waiting for supplier',
+	'Class:UserRequest/Attribute:status/Value:attente_client' => 'Waiting for customer',
 ));
 ?>
---
+++ fr.dict.oneytrust-exercice.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Localized data
+ *
+ * @copyright   Copyright (C) 2013 XXXXX
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+Dict::Add('FR FR', 'French', 'Français', array(
+	// Dictionary entries go here
+	'Class:UserRequest/Stimulus:ev_attente_client' => 'En attente du client',
+	'Class:UserRequest/Stimulus:ev_attente_fournisseur' => 'En attente du fournisseur',
+	'Class:UserRequest/Stimulus:ev_attente_atelier' => 'En attente de l\'atelier',
+	'Class:UserRequest/Attribute:status/Value:attente_atelier' => 'En attente de l\'atelier',
+	'Class:UserRequest/Attribute:status/Value:attente_fournisseur' => 'En attente du fournisseur',
+	'Class:UserRequest/Attribute:status/Value:attente_client' => 'En attente du client',
+));
+?>
